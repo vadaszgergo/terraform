2870553a939de6fa18d84f1ef352c50a8f735d93
Gergo Vadasz <gergo@MacBook-Pro-4.local>
2024-02-04 22:00:22 +0100

    change cidr

--- gcp/main.tf
+++ gcp/main.tf
@@ -21,7 +21,7 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_subnetwork" "subnet01" {
   name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.2.0.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
 }
